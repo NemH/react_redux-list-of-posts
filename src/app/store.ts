6a9ed22bc4da992c0eb4c6fb4b0cf02459b4a78b
Maksum Nemchenko <maksnemch2004@gmail.com>
2026-05-21 16:44:51 +0300

    Solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,7 +4,7 @@ import authorSlice from '../features/author/authorSlice';
 import UsersSlice from '../features/users/usersSlice';
 import postsSlice from '../features/posts/postsSlice';
 import selectedPostSlice from '../features/selectedPost/selectedPostSlice';
-import commentsSlice from '../features/comments/commentsSliceSlice';
+import commentsSlice from '../features/comments/commentsSlice';
 
 export const store = configureStore({
   reducer: {
